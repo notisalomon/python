a7f1982b0043117df3dc707d7edffdd0683f10f8
notisalomon <sulejmani.labinot@gmail.com>
2022-11-11 09:38:04 +0100

    changed the message

---
+++ Altervorhersage /Alter.py
@@ -0,0 +1,10 @@
+print("Mal schauen, wie lange du in Tagen, Minuten und Sekunden lebst.")
+Name = input("Name: ")
+print("Gib jetzt dein Alter ein")
+Alter = int(input("Alter: "))
+
+Tagen = Alter * 365
+Minuten = Alter * 525948
+Sekunden = Alter * 31556926
+
+print(Name, "lebt schon seit", Tagen,"Tagen,", Minuten, "Minuten und", Sekunden, "Sekunden!  Wow, was für ein alter Sack!")
