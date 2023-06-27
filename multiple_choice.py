b64a87b4f3a0fc2a5d293facd4b2a87d6a76175e
notisalomon <sulejmani.labinot@gmail.com>
2023-06-27 13:50:08 +0200

    refined the whole program

--- multiple_choice.py
+++ multiple_choice.py
@@ -9,20 +9,20 @@ question1 = {
 time.sleep(0.5)
 # Print the question and answer choices
 print(question1["question"])
-time.sleep(1.5)
+time.sleep(0.9)
 for choice in question1["choices"]:
         print(choice)
 
-time.sleep(2)
+time.sleep(0.5)
 
 # Ask the user to select an answer
 user_answer = input("Enter your answer (A, B or C): ")
 
-time.sleep(0.6)
+time.sleep(0.5)
 
 # Check if the answer is correct
 if user_answer == question1["answer"]:
-	print("Correct")
+	print('\x1b[1;32;40m' + "Correct" + '\x1b[0m')
 else:
-	print("Sorry, tha'ts incorrect.")
-time.sleep(1)
+	print('\x1b[1;31;40m' + "Sorry, that's incorrect." + '\x1b[0m')
+time.sleep(0.6)
