876f6f0ed663e1360d169d2b52f8a02ba28d9c84
notisalomon <sulejmani.labinot@gmail.com>
2023-06-27 12:13:16 +0200

    Imported time sleep function in multiple choice and changed the files structure

---
+++ README.md
@@ -0,0 +1,2 @@
+# Python
+Welcome to my Python projects page! Here you find several practice and personal projects of mine. Feel free to clone the repository and have fun😉!
--- multiple_choice.py
+++ multiple_choice.py
@@ -1,20 +1,28 @@
+import time
+
 question1 = {
 	"question": "What is the capital of France?",
 	"choices": ["A. Paris", "B. London", "C. Berlin"],
 	"answer": "A"
 }
 
+time.sleep(0.5)
 # Print the question and answer choices
 print(question1["question"])
+time.sleep(1.5)
 for choice in question1["choices"]:
         print(choice)
 
+time.sleep(2)
+
 # Ask the user to select an answer
 user_answer = input("Enter your answer (A, B or C): ")
+
+time.sleep(0.6)
 
 # Check if the answer is correct
 if user_answer == question1["answer"]:
 	print("Correct")
 else:
 	print("Sorry, tha'ts incorrect.")
-
+time.sleep(1)
