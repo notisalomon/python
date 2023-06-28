702fe7abebfb5bff77957596c5f4cdd17be2924f
notisalomon <sulejmani.labinot@gmail.com>
2023-06-28 13:26:55 +0200

    finished the age program for all scenarios

--- age.py
+++ age.py
@@ -1,8 +1,9 @@
 age = int(input("How old are you? "))
 
-if age >= 18:
-    if age == 18:
-        print("You are exactly 18")
-    else:
-        print("You are older than 18 years old")
+if age < 18:
+    print("You are below 18")
+elif age == 18:
+    print("You are exactly 18")
+else:
+    print("You are older than 18 years old")
 
