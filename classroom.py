a0cc717af3e49689326785aacce07327f090d0dc
notisalomon <sulejmani.labinot@gmail.com>
2023-06-30 15:40:24 +0200

    Created 2d and 3d lists

---
+++ classroom.py
@@ -0,0 +1,30 @@
+classroom = [
+        [
+            ["Sam", "Max", "Joe", "Anne"],
+            ["Sofie", "Lisa", "Tim", "Sasha"],
+            ["Claire", "Sara", "Leo", "Kim"],
+            ["Zoe", "Guy", "Anna", "Eva"],
+        ],
+        [
+            ["Gabriel", "Toni", "Maria", "Eva"],
+            ["Jimmy", "Alex", "Antonio", "Malia"],
+            ["Svetlana", "Meggie", "Julian", "Chloe"],
+            ["Brent", "Sam", "Sarah", "Anne"],
+        ],
+        [
+            ["Maria", "Julian", "Chris", "Tom"],
+            ["Zoe", "Anna", "Kim", "Leo"],
+            ["Vera", "Pim", "Leo", "Guys"],
+            ["Anne", "Sofie", "Max", "Joe"],
+        ],
+        [
+            ["Sam", "Max", "Joe", "Anne"],
+            ["Sofie", "Lisa", "Tim", "Sasha"],
+            ["Claire", "Sara", "Leo", "Kim"],
+            ["Zoe", "Guy", "Anna", "Eva"],
+        ]
+    ]
+
+student = classroom[2][0][0]
+
+print(student)
---
+++ lists.py
@@ -0,0 +1,4 @@
+fruits = ["Apples", "Ananas", "Banana", "Avocado"]
+First2Fruits = fruits[0:2]
+print(First2Fruits)
+print(fruits[:])
