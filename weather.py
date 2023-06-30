1d1cdf6b46cd672210165088dc3683aec9dd8369
notisalomon <sulejmani.labinot@gmail.com>
2023-06-30 08:18:34 +0200

    refined the program by making the user an offer

--- weather.py
+++ weather.py
@@ -1,4 +1,20 @@
 import time
+import webbrowser
+import sys
+
+manor_url = 'www.manor.ch'
+galaxus_url = 'www.galaxus.ch'
+
+manor = {
+        "question": "Are you interested? [Yes] or [No]: ",
+        "Yes": ["Yes", "yes"],
+        "No": ["No", "no"]
+        }
+galaxus = {
+        "question": "You want to take a look? [Yes] or [No]: ",
+        "Yes": ["Yes", "yes"],
+        "No": ["No", "no"]
+        }
 
 temp = int(input("How is the temperature today?: "))
 time.sleep(0.4)
@@ -7,15 +23,28 @@ if temp >= 0 and temp <= 12:
     time.sleep(2.4)
     print("I saw that Manor offers some really warm jackets for a discounted price.")
     time.sleep(2.2)
-    offer = input("Are you interested?: ")
-elif temp <= 13 or temp <= 30:
+    print(manor["question"])
+    manor_answer = input("")
+    if manor_answer == "Yes":
+        webbrowser.open_new_tab(manor_url)
+elif temp >= 13 or temp >= 30:
     print("Oh wow, seems to be nice out there. What are you doing here? Go out!")
     time.sleep(2.6)
     print("Galaxus has some great offers for rubber boats.")
     time.sleep(1.2)
-    offer_galaxus = input("You want to take a look?: ")
-
+    print(galaxus["question"])
+    galaxus_answer = input("")
+    if galaxus_answer == "Yes":
+        webbrowser.open_new_tab(galaxus_url)
 time.sleep(0.5)
+
+# Here the program forwards the user to the manor page or it exits
+#if manor_answer == "Yes":
+#    webbrowser.open_new_tab(manor_url)
+#else:
+#    print("Alright, have a good time")
+#galaxus_answer == "Yes":
+    #webbrowser.open_new_tab(galaxus_url)
 
 # if temp >= 0 and temp <= 12:
     # print("I saw that Manor offers some really warm jackets for a discounted price.")
