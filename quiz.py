b974d9b5f409f1f2bcfbf6a7029b2f846931c7b9
notisalomon <sulejmani.labinot@gmail.com>
2023-07-03 14:57:56 +0200

    Created a program that prints the exact argument you provide

---
+++ quiz.py
@@ -0,0 +1,11 @@
+def my_function(*argv):
+    print(argv)
+my_function("Hello", "World", "iam just doing some test", 123564)
+
+#x = 20
+#def my_function():
+#    x = 30
+#    print(x, end='')
+
+#my_function()
+#print(x, end='')
