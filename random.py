c132243bafdf235a41fd50fd21aeb27a728c13bb
notisalomon <sulejmani.labinot@gmail.com>
2023-08-24 14:21:47 +0200

    automate the boring stuff with python

---
+++ random.py
@@ -0,0 +1,29 @@
+def plusOne (number):
+    return number + 1
+
+newNumber = plusOne(5)
+print(newNumber)
+
+#______
+#def hello():
+#    print("Howdy")
+#    print("Howdyyy!!")
+#    print("Howdyyy!!!!!!")
+
+#hello()
+#hello()
+#hello()
+#______
+
+#______
+#import pyperclip
+#pyperclip.copy("Hello World!")
+#pyperclip.paste()
+#______
+
+#______
+#import sys
+#print("HEllo")
+#print("Byee")
+#sys.exit()
+#______
