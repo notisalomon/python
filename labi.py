9b06e4e9c193c6905a6153f783d3cd891d4aa093
notisalomon <sulejmani.labinot@gmail.com>
2023-08-25 16:16:37 +0200

    created some additional practice programs

---
+++ labi.py
@@ -0,0 +1,11 @@
+import time
+
+print("Hi there, how are you doing?")
+time.sleep(1)
+name = input("What is your name?: ")
+print("Nice to meet you " + name)
+time.sleep(0.5)
+country = input("Where are you from " + name + "?: ")
+time.sleep(0.5)
+print("Oh lovely.")
+time.sleep(0.5)
