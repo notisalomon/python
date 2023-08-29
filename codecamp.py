10fae8f9783d62561e4e8eacacf3f5fa1e6a1a04
notisalomon <sulejmani.labinot@gmail.com>
2023-08-29 18:57:07 +0200

    practiced some functions and variables

--- codecamp.py
+++ codecamp.py
@@ -1,4 +1,43 @@
-print(225)
+
+
+#Lists & Lists Functions
+#friends = ["Kevin", "Karen", "Jim", "Suzana"]
+#friends[1] = "Mike"
+#friends.remove("Kevin")
+#print(friends.index("Mike"))
+#friends.sort()
+#print(friends)
+#friends2 = friends.copy()
+#print(friends2)
+
+
+#Madlip Game
+#color = input("Enter a color: ")
+#plural_noun = input("Enter a Plural Noun: ")
+#celebrity = input("Enter a celebrity: ")
+#print("Roses are " + color)
+#print(plural_noun + " are blue")
+#print("I love " + celebrity)
+
+#Calculator
+#num1 = float(input("Enter a number: "))
+#num2 = float(input("Enter another number: "))
+#result = num1 + num2
+#print(result)
+
+
+#User Inputs + Variables
+#name = input("Enter your name: ")
+#age = input("Enter your age: ")
+#print("Hello " + name + "! You are " + age + " years old!")
+
+#Working with Numbers
+#from math import *
+#my_num=-5
+#print(sqrt(36))
+#print(ceil(2.1))
+#print(min(4, 6, 5, 9, 12, 1))
+#print(str(my_num) + " is my favorite number!")
 
 #Working with Strings
 #phrase = "Giraffe Academy"
