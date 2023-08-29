132b7d2ef1b04771b79662b361e15a3468fea134
notisalomon <sulejmani.labinot@gmail.com>
2023-08-29 16:49:44 +0200

    Working with variables

--- codecamp.py
+++ codecamp.py
@@ -1,4 +1,15 @@
-import pandas as pd
 
-df_premier24 = pd.read_csv('https://football-data.co.uk/mmz4281/2324/E0.csv')
-df_premier24
+
+
+
+
+
+#character_name = "Labi"
+#age = "28"
+#is_male = True
+
+#print("There once was a man named " + character_name + ",")
+#print("he was " + age + " years old. ")
+#character_name = "Toni"
+#print("He really liked the name " + character_name + ",")
+#print("but didn't like being " + age + ".")
