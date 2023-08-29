34c0f3c3a5ee65a8b3c6cd5558a362eb4f36b069
notisalomon <sulejmani.labinot@gmail.com>
2023-08-29 16:18:44 +0200

    added some funcitons to the lists program

--- lists.py
+++ lists.py
@@ -2,3 +2,9 @@ fruits = ["Apples", "Ananas", "Banana", "Avocado"]
 First2Fruits = fruits[0:2]
 print(First2Fruits)
 print(fruits[:])
+
+
+for i in range(len(fruits)):
+    print("Index " + str(i) + " in fruits is: " + fruits[i])
+
+fruits.index("Ananas")
