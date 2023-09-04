f774a102b1e85bf55f5580f1b4f94ecdb0f95ee7
notisalomon <sulejmani.labinot@gmail.com>
2023-09-04 11:38:24 +0200

    created a guessing game

--- codecamp.py
+++ codecamp.py
@@ -1,17 +1,62 @@
-#Advanced Calculator
-num1 = float(input("Enter first number: "))
-op = input("Enter operator: ")
-num2 = float(input("Enter second number: "))
-if op == "+":
-    print(num1 + num2)
-elif op == "-":
-    print(num1 - num2)
-elif op == "/":
-    print(num1 / num2)
-elif op == "*":
-    print(num1 * num2)
+#Building a Guessing Game
+secret_word = "giraffe"
+guess = ""
+guess_count = 0
+guess_limit = 3
+out_of_guesses = False
+
+while guess != secret_word and not(out_of_guesses):
+    if guess_count < guess_limit:
+        guess = input("Enter guess: ")
+        guess_count += 1
+    else:
+        out_of_guesses = True
+
+if out_of_guesses:
+    print("Out of Guesses, YOU LOOSE!")
 else:
-    print("Invalid Operator")
+    print("You win!")
+
+
+#while loops
+#i = 1
+#while i <= 10:
+#    print(i)
+#    i += 1
+#
+#print("Done with loop")
+
+#Dictionaries
+#monthConversions = {
+#    "Jan": "January",
+#    "Feb": "February",
+#    "Mar": "March",
+#    "Apr": "April",
+#    "May": "May",
+#    "Jun": "June",
+#    "Jul": "July",
+#    "Aug": "August",
+#    "Sep": "September",
+#    "Oct": "October",
+#    "Nov": "November",
+#    "Dec": "December",
+#}
+#print(monthConversions.get("Luv", "Not a valid Key"))
+
+#Advanced Calculator
+#num1 = float(input("Enter first number: "))
+#op = input("Enter operator: ")
+#num2 = float(input("Enter second number: "))
+#if op == "+":
+#    print(num1 + num2)
+#elif op == "-":
+#    print(num1 - num2)
+#elif op == "/":
+#    print(num1 / num2)
+#elif op == "*":
+#    print(num1 * num2)
+#else:
+#    print("Invalid Operator")
 
 #if statements and comparisons
 #def max_num(num1, num2, num3):
